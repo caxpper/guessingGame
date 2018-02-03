bb0bebb8b5e12517eadb5bda0437cd6d38c60b25
Alejandro Gaspar <caxpper@hotmail.com>
2018-02-02 18:24:09 -0800

    index and end pages added. Added monsters

--- main.js
+++ main.js
@@ -57,29 +57,47 @@ function createDoor(index){
         openDoor(this);
     });
     doorBorder.append(door);
+    doorBorder.append($("<div>",{
+        class: "monster",
+        css:
+            {
+                background: 'url('+getMonster()+')',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center'
+            }
+    }));
     return doorBorder;
+}
+
+function getMonster(){
+    var monsters = ["images/sully.png","images/bile.png","images/celia.png","images/George.png","images/Randall.png","images/Mike.png"];
+    var index = Math.floor(Math.random() * 5);
+    return monsters[index];
 }
 
 function openDoor(field) {
     var y = $(field).find(".thumb");
-    var x = y.attr("class");
+    var monster = y.parent().find(".monster");
     if (y.hasClass("thumbOpened")) {
         y.removeClass("thumbOpened");
+        monster.hide();
     }
     else {
         $(".thumb").removeClass("thumbOpened");
+        $(".monster").hide();
         y.addClass("thumbOpened");
 
         var the_guess = y.attr('id');
         if(the_guess > the_number){
             $("#response_div").text("She is not here. I saw her more to the left!");
+            monster.show();
         }else if(the_guess < the_number){
             $("#response_div").text("She is not here. I saw her more to the right!");
+            monster.show();
         }else if(the_guess == the_number){
             y.parent().append($("<div>").addClass("boo"));
             $("#response_div").text("You found her!");
-            $("#response_div").css({'font-weight':'bold',
-                'font-size': 'larger'});
+            setTimeout(function(){location.href="end.html"} , 2000);
         }
     }
 }
